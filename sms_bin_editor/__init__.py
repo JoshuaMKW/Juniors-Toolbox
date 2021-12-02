16394860a3b12d6f60195d519e03ea97fc665579
JoshuaMK <joshuamkw2002@gmail.com>
2021-12-01 23:59:31 -0600

    Work on objects

--- sms_bin_editor/__init__.py
+++ sms_bin_editor/__init__.py
@@ -2,5 +2,6 @@ __version__ = "0.0.1"
 __author__ = "JoshuaMK"
 __credits__ = {
     "Augs": "https://github.com/AugsEU",
-    "Yoshi2": "https://github.com/RenolY2"
+    "Yoshi2": "https://github.com/RenolY2",
+    "tarsa129": "https://github.com/tarsa129",
 }
